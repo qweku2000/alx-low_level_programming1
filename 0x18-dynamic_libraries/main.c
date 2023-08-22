c1376120cb0453735b634d1c03dbbaf7164b1724
qweku2000 <joeadofonyarko@gmail.com>
2023-08-21 18:52:22 -0700

    Compiled into library

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to the standard output.
+ * @c: The character to be written.
+ *
+ * Return: Always 0.
+ */
+int _putchar(char c)
+{
+    return 0;
+}
+
+/**
+ * _islower - Checks if a character is a lowercase letter.
+ * @c: The character to be checked.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+    return 0;
+}
+
+/**
+ * _isalpha - Checks if a character is an alphabetic character.
+ * @c: The character to be checked.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+    return 0;
+}
+
+/**
+ * _abs - Calculates the absolute value of an integer.
+ * @n: The integer for which to calculate the absolute value.
+ *
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+    return 0;
+}
+
+/**
+ * _isupper - Checks if a character is an uppercase letter.
+ * @c: The character to be checked.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+    return 0;
+}
+
+/**
+ * _isdigit - Checks if a character is a digit.
+ * @c: The character to be checked.
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+    return 0;
+}
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+    return 0;
+}
+
+/**
+ * _puts - Writes a string to the standard output.
+ * @s: The string to be written.
+ */
+void _puts(char *s)
+{
+    /* This is an empty function. */
+}
+
+/**
+ * _strcpy - Copies a string from source to destination.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ *
+ * Return: The address of the destination buffer.
+ */
+char *_strcpy(char *dest, char *src)
+{
+    return dest;
+}
+
+/* Continue with the rest of the functions in a similar format */
