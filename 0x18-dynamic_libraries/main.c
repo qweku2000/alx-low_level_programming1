5bb855274d9b7bd529505f999ee0a79fa5fc13f6
qweku2000 <joeadofonyarko@gmail.com>
2023-08-21 18:58:37 -0700

    Completion f Task 0

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -99,3 +99,91 @@ char *_strcpy(char *dest, char *src)
 }
 
 /* Continue with the rest of the functions in a similar format */
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+    return 0;
+}
+
+/**
+ * _memset - Fills a block of memory with a specific value.
+ * @s: The memory block to fill.
+ * @b: The value to set.
+ * @n: The number of bytes to fill.
+ *
+ * Return: Always NULL.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    return 0;
+}
+
+/**
+ * _memcpy - Copies a block of memory from source to destination.
+ * @dest: The destination memory.
+ * @src: The source memory.
+ * @n: The number of bytes to copy.
+ *
+ * Return: Always NULL.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    return 0;
+}
+
+/**
+ * _strchr - Locates the first occurrence of a character in a string.
+ * @s: The string to search.
+ * @c: The character to locate.
+ *
+ * Return: Always NULL.
+ */
+char *_strchr(char *s, char c)
+{
+    return 0;
+}
+
+/**
+ * _strspn - Calculates the length of a prefix substring that consists
+ *           of characters from a given set.
+ * @s: The string to be scanned.
+ * @accept: The set of characters to search for.
+ *
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+    return 0;
+}
+
+/**
+ * _strpbrk - Locates the first occurrence of any character from a given
+ *            set in a string.
+ * @s: The string to be scanned.
+ * @accept: The set of characters to search for.
+ *
+ * Return: Always NULL.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    return 0;
+}
+
+/**
+ * _strstr - Locates the first occurrence of a substring in a string.
+ * @haystack: The string to be searched.
+ * @needle: The substring to search for.
+ *
+ * Return: Always NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    return 0;
+}
